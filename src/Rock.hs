106fc46750bae5b1b48b1a1eb096c18413b9a888
AAYUSH ARORA <ee17btech11003@iith.ac.in>
2019-04-07 00:30:53 +0530

    Add files via upload

---
+++ src/Rock.hs
@@ -0,0 +1,15 @@
+module Rock where
+
+import qualified Animate
+import Data.Text (Text)
+
+data RockKey
+  = RockKey'Idle
+  deriving (Show, Eq, Ord, Bounded, Enum)
+
+instance Animate.KeyName RockKey where
+  keyName = rockKey'keyName
+
+rockKey'keyName :: RockKey -> Text
+rockKey'keyName = \case
+  RockKey'Idle -> "Idle"
