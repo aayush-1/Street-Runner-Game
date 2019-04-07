667e00c3b7ac67bd513075fec85b43e7778c0835
poojaMate14 <45240902+poojaMate14@users.noreply.github.com>
2019-04-07 19:21:19 +0530

    Create Sfx.hs

---
+++ src/Sfx.hs
@@ -0,0 +1,14 @@
+module Sfx where
+
+data Sfx
+  = Sfx'Jump
+  | Sfx'Point
+  | Sfx'Duck
+  | Sfx'Bird
+  | Sfx'Hurt
+  | Sfx'Lava
+  | Sfx'Quake
+  | Sfx'Rock
+  | Sfx'Recover
+  | Sfx'Stock
+  deriving (Show, Eq)
