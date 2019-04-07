cd1ad36bb490642e6d77e062149ffecfbf989e9d
aarushicp <43223110+aarushicp@users.noreply.github.com>
2019-04-07 18:59:18 +0530

    Create Bird.hs

---
+++ src/Bird.hs
@@ -0,0 +1,15 @@
+module StreetRunner.Engine.Bird where
+
+import Data.Text (Text)
+import qualified Animate
+
+data BirdKey
+  = BirdKey'Idle
+  deriving (Show, Eq, Ord, Bounded, Enum)
+
+instance Animate.KeyName BirdKey where
+  keyName = birdKey'keyName
+
+birdKey'keyName :: BirdKey -> Text
+birdKey'keyName = \case
+  BirdKey'Idle -> "Idle"
