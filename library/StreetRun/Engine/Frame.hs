2f8d922c3de081d008454eedd27def8ba9653edd
aak-ash <aakashdaswani@gmail.com>
2019-04-14 01:33:34 +0530

    visuals

---
+++ library/StreetRun/Engine/Frame.hs
@@ -0,0 +1,7 @@
+module StreetRun.Engine.Frame where
+
+frameDeltaSeconds :: Fractional a => a --time it takes to chaneg the frames 
+frameDeltaSeconds = 0.016667
+
+frameDeltaMilliseconds :: Int
+frameDeltaMilliseconds = 20 --time it takes to change the frame 
